63353a3ee77801fef4b985d71e9927f1c31eb58f
andycole27 <rtofaig@gmail.com>
2020-08-07 03:18:39 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,20 +8,26 @@ const bodyParser = require('body-parser');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const flash = require('connect-flash');
 const csrf = require('csurf');
-const stemgirl = require('./controller/stemgirl');
-
-const error = require('./controller/error');
+const stemgirl = require('./controllers/stemgirl');
+const homeRoutes = require('./routes/home');
+const ambassadorRoutes = require('./routes/ambassador');
+app.use(homeRoutes);
+app.use(ambassadorRoutes);
+const error = require('./controllers/error');
 //  //local imports
 const isAuth = require('./middleware/is-auth');
 app.use('http://localhost:3000/images', express.static(path.join(__dirname, 'images')));
 const User = require('./models/user');
-app.use((req, res, next) => {
-  //res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
+// app.use((req, res, next) => {
+//   //res.locals.isAuthenticated = req.session.isLoggedIn;
+//   res.locals.csrfToken = req.csrfToken();
+//   next();
+// });
 app.use(flash());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const MONGODB_URI = 'mongodb://localhost:27017/stem?connectTimeoutMS=10';
  const store = new MongoDBStore({
@@ -36,7 +42,12 @@ app.use(
    store:store
   })
  );
- const csrfProtection = csrf();
+ //Configuring Express middleware for the handlebars
+app.set('views', path.join(__dirname, '/views/'));
+
+
+app.set('view engine', 'ejs');
+ //const csrfProtection = csrf();
 //PORT ENVIRONMENT VARIABLE
 const port = process.env.PORT || 27017;
 mongoose
